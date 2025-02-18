24546e0447d3d7f842a99a7301bc29a526831f9e
juan Asensio <juan.falco21@gamil.com>
2025-02-18 07:55:36 -0300

    Reapply "pushing first project"
    This reverts commit c1ffe123f69e7118d3df614539b248556439bef0.

---
+++ iniciando/target/debug/deps/iniciando-77eb5d871734595a.d
@@ -0,0 +1,5 @@
+C:\Users\juanf\Desktop\rustyFullStack\iniciando\target\debug\deps\libiniciando-77eb5d871734595a.rmeta: src/main.rs
+
+C:\Users\juanf\Desktop\rustyFullStack\iniciando\target\debug\deps\iniciando-77eb5d871734595a.d: src/main.rs
+
+src/main.rs:
