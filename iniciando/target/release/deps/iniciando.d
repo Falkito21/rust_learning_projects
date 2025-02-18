300b12881219a58c896b11f1f7148568b93fa000
juan Asensio <juan.falco21@gamil.com>
2025-02-18 07:51:57 -0300

    pushing first project

---
+++ iniciando/target/release/deps/iniciando.d
@@ -0,0 +1,5 @@
+C:\Users\juanf\Desktop\rustyFullStack\iniciando\target\release\deps\iniciando.exe: src/main.rs
+
+C:\Users\juanf\Desktop\rustyFullStack\iniciando\target\release\deps\iniciando.d: src/main.rs
+
+src/main.rs:
