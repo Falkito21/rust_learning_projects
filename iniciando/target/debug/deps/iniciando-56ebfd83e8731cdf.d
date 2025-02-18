3ae26648a72d065c2e0f05c150a7a2668ba2b369
juan Asensio <juan.falco21@gamil.com>
2025-02-18 07:51:57 -0300

    pushing first project

---
+++ iniciando/target/debug/deps/iniciando-56ebfd83e8731cdf.d
@@ -0,0 +1,5 @@
+C:\Users\juanf\Desktop\rustyFullStack\iniciando\target\debug\deps\libiniciando-56ebfd83e8731cdf.rmeta: src/main.rs
+
+C:\Users\juanf\Desktop\rustyFullStack\iniciando\target\debug\deps\iniciando-56ebfd83e8731cdf.d: src/main.rs
+
+src/main.rs:
---
+++ iniciando/target/debug/deps/iniciando-a87479904ec2349a.d
@@ -0,0 +1,5 @@
+C:\Users\juanf\Desktop\rustyFullStack\iniciando\target\debug\deps\libiniciando-a87479904ec2349a.rmeta: src/main.rs
+
+C:\Users\juanf\Desktop\rustyFullStack\iniciando\target\debug\deps\iniciando-a87479904ec2349a.d: src/main.rs
+
+src/main.rs:
